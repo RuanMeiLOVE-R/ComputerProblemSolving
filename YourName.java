55b56154c2bdeefe0e24ca0fafac5425799289fb
RuanMeiLOVE-R <>
2026-06-25 16:07:40 +0700

    origin

---
+++ YourName.java
@@ -0,0 +1,7 @@
+void main() {
+    IO.println("Enter Name");
+    String name = IO.readln();
+    IO.println("Enter Age");
+    int age = Integer.parseInt(IO.readln());
+    IO.println(" Long time to see. " + name + " You look so much more mature. Is it really " + age + " Time really flies.");
+}
